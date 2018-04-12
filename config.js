319c9d0db6898dbcc90088b9d22808b1abff2968
mfrro <m.follonier@rro.ch>
2018-04-12 09:31:27 +0200

    redis config

--- config.js
+++ config.js
@@ -23,6 +23,6 @@ exports.googleanalytics = {
 exports.database = {
 	type: 'redis',
 	prefix: '#scrumblr#',
-	redis: argv.redis || 'redis://redis-18895.c15.us-east-1-4.ec2.cloud.redislabs.com:18895'
+	redis: argv.redis || process.env.REDISCLOUD_URL
 };
 
