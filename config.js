44b200ac7472679c9680bcb1a0eac016512a0fe3
mfrro <m.follonier@rro.ch>
2018-04-12 09:11:59 +0200

    heroku redis database

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var argv = require('yargs')
 
 exports.server = {
 	port: argv.port || process.env.PORT || 8080,
-	baseurl: argv.baseurl
+	baseurl: argv.baseurl || 'scrumblr'
 };
 
 exports.googleanalytics = {
@@ -23,6 +23,6 @@ exports.googleanalytics = {
 exports.database = {
 	type: 'redis',
 	prefix: '#scrumblr#',
-	redis: argv.redis || 'redis://127.0.0.1:6379'
+	redis: argv.redis || 'redis://redis-18895.c15.us-east-1-4.ec2.cloud.redislabs.com:18895'
 };
 
